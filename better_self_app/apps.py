ed1c2eab3f79013f3961629b7575d9f33106dbfb
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-09-13 14:59:22 -0600

    Cleared cache

--- better_self_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BetterSelfAppConfig(AppConfig):
-    name = 'better_self_app'
--- login_registration_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.admin), #GET request to display registration and login form
-    path('/register', views.register), #POST request to register user
-    path('/login', views.login), #POST request to login user
-    path('/logout', views.logout), #POST request to logout user,
-]
